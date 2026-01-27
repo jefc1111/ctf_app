b5463e3140e4966024f4252c9901b1d87baa5a33
Geoff Clayton <jefc_uk@hotmail.com>
2026-01-27 21:45:55 +0000

    Add initial bits for Submissions

---
+++ database/migrations/2026_01_27_213631_create_submissions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('submission_category_id');
+            $table->foreignId('team_id');
+            $table->foreignId('event_id');
+            $table->foreignId('case_id');
+            $table->string('name');
+            $table->string('content');
+            $table->string('explanation');
+            $table->boolean('draft');
+            $table->string('decision_status');
+            $table->string('decision_supporting_evidence');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submissions');
+    }
+};
