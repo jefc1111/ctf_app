68bc9818afbf44ccdd7c6e67ca95d79d9a6768d7
Geoff Clayton <jefc_uk@hotmail.com>
2026-01-27 21:34:20 +0000

    Submission Category form

--- app/Filament/Resources/SubmissionCategories/Schemas/SubmissionCategoryForm.php
+++ app/Filament/Resources/SubmissionCategories/Schemas/SubmissionCategoryForm.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Filament\Resources\SubmissionCategories\Schemas;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 
 use Filament\Schemas\Schema;
 
@@ -10,7 +12,13 @@ class SubmissionCategoryForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('points')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpan(2)
+                    ->rows(5)
             ]);
     }
 }
