1a215342b282316250af6ca44254c5137d18b9c9
Geoff Clayton <jefc_uk@hotmail.com>
2026-01-28 04:46:08 +0000

    Make various entities auditable

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Auditable as AuditableTrait;
 
 class Submission extends Model
 {
     use SoftDeletes;
+    use AuditableTrait;
     //
 }
