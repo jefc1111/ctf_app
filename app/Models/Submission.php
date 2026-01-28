1f026ee765eafcf062b35d9f422dc60d78b5daaf
Geoff Clayton <jefc_uk@hotmail.com>
2026-01-28 12:28:09 +0000

    Add Submissions factory and seeder

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use OwenIt\Auditing\Auditable as AuditableTrait;
 
 class Submission extends Model
 {
     use SoftDeletes;
     use AuditableTrait;
+    use HasFactory;
     //
 }
--- database/factories/SubmissionFactory.php
+++ database/factories/SubmissionFactory.php
@@ -17,7 +17,18 @@ class SubmissionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->word(),
+            'content' => fake()->paragraph(),
+            'explanation' => fake()->paragraph(),
+            'draft' => fake()->boolean(20), // The arg tweaks the likelihood of getting `true`
+            'decision_status' => fake()->randomElement([
+                'PENDING', 
+                'UNDER_REVIEW', 
+                'AWAITING_PEER_REVIEW',
+                'APPROVED',
+                'DECLINED'
+            ]),
+            'decision_supporting_evidence' => fake()->paragraph()
         ];
     }
 }
