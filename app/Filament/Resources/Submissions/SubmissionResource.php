8fefd3b626dd006453c331a5ba99add14be9f0d2
Geoff Clayton <jefc_uk@hotmail.com>
2026-01-29 10:52:21 +0000

    Add Filament package for showing audits

--- app/Filament/Resources/Submissions/SubmissionResource.php
+++ app/Filament/Resources/Submissions/SubmissionResource.php
@@ -18,6 +18,7 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Tapp\FilamentAuditing\RelationManagers\AuditsRelationManager;
 
 class SubmissionResource extends Resource
 {
@@ -49,7 +50,7 @@ class SubmissionResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            AuditsRelationManager::class
         ];
     }
 
