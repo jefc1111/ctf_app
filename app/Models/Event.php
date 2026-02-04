9b25e8d552e55b5c06eb850c74f8dfd5685ff220
Geoff Clayton <jefc_uk@hotmail.com>
2026-02-04 20:23:38 +0000

    Add various Utility methods to the Event class

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Auditable as AuditableTrait;
+use Carbon\Carbon;
 
 class Event extends Model implements Auditable
 {
@@ -21,6 +22,19 @@ class Event extends Model implements Auditable
         'end_time'
     ];
 
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'start_time' => 'datetime', // 'datetime:Y-m-d'
+            'end_time' => 'datetime',
+        ];
+    }
+
     public function cases(): HasMany
     {
         return $this->hasMany(CaseModel::class);
@@ -29,5 +43,46 @@ class Event extends Model implements Auditable
     public function teams(): HasMany
     {
         return $this->hasMany(Team::class);
+    }
+
+    public function isPending(): bool
+    {
+        return $this->start_time > Carbon::now();
+    }
+
+    public function isInProgress(): bool
+    {
+        return (! $this->isPending()) && (! $this->isEnded());
+    }
+
+    public function isEnded(): bool
+    {
+        return $this->end_time < Carbon::now();
+    }
+
+    public function progressStatusText(): string
+    {
+        if ($this->isPending()) {
+            return 'Pending';
+        }
+
+        if ($this->isInProgress()) {
+            return 'In Progress';
+        }
+
+        return 'Complete';
+    }
+
+    public function countdownTimerLabel(): string
+    {
+        if ($this->isPending()) {
+            return 'Starts in';
+        }
+
+        if ($this->isInProgress()) {
+            return 'Ends in';
+        }
+
+        return 'Event complete';
     }
 }
