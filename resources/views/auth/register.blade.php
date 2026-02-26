142dd683ae030226ba89f1dba8549ab8e4b6a5ff
Geoff Clayton <jefc_uk@hotmail.com>
2026-02-26 19:33:21 +0000

    Update jetstream views to look similar to Filament

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <img width="256" src="{{ asset('logos/ctf-logo.png') }}" alt="Logo">
+            <!-- <x-authentication-card-logo /> -->
         </x-slot>
 
         <x-validation-errors class="mb-4" />
